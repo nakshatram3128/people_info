f338bfc179cb27a442a267926139279703e48c26
Naveen Kumar <naveenkumar@Naveens-MacBook-Pro-2.local>
2023-12-15 16:47:33 +0000

    test_app

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
 from aiohttp import web
 
+async def sample(request):
+    return web.Response(text="Hello, this is your aiohttp sample endpoint!")
+
 async def handle(request):
     return web.Response(text="Hello, this is your aiohttp endpoint!")
 
 app = web.Application()
 app.router.add_get('/', handle)
+app.router.add_get('/sample', sample)
 
 if __name__ == '__main__':
     print("I am In")
